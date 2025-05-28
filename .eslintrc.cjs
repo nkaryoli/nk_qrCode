222ae5e3cb4191817da0fa4b0597fbf4eb0fbe20
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-28 22:36:54 +0200

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+		'prettier'
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh', '@typescript-eslint'],
+	rules: {
+		'react-refresh/only-export-components': [
+		'warn',
+		{ allowConstantExport: true },
+		],
+		'@typescript-eslint/no-unused-vars': 'error',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+	},
+	settings: {
+		react: {
+		version: 'detect',
+		},
+	},
+}
