9862f10c0adcd4928555d1d268287a8d5c2dc011
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-29 19:50:34 +0200

    feat: move  modeToggle component to navbar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { useScrollToTop } from '@/hooks/useScrollToTop';
 import { Outlet } from 'react-router-dom';
 import { ThemeProvider } from '../../hooks/theme-provider';
-import { ModeToggle } from '../ModeToggle';
+import Header from './header/Header';
 
 const Layout = () => {
     useScrollToTop();
@@ -9,9 +9,7 @@ const Layout = () => {
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
             <div className="grid-background"></div>
             <main className="min-h-screen flex flex-col items-center justify-between">
-                <div className="ml-4">
-                    <ModeToggle />
-                </div>
+                <Header />
                 <Outlet />
             </main>
         </ThemeProvider>
