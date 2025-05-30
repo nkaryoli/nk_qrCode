69d35ff30f67b9bf8af5f4cc8928570f6341d78e
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-05-30 19:42:40 +0200

    feat: add QRPreview component

---
+++ src/features/custom/components/QRPreview.tsx
@@ -0,0 +1,68 @@
+import QRDisplay from '@/components/qrCode/QRDisplay';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { useIsMobile } from '@/hooks/useIsMobile';
+import { DownloadIcon, HeartPlus } from 'lucide-react';
+import { memo } from 'react';
+import { useQR } from '@/hooks/QRContext';
+import { useQRManager } from '@/hooks/useQRManager';
+import QRPreviewModal from './QRPreviewModal';
+import QREmptyState from './QREmptyState';
+
+const QRPreview = () => {
+    const { isSaving, handleDownload, handleSaveQRCode } = useQRManager();
+    const { title, qrRef, qrConfig } = useQR();
+    const isMobile = useIsMobile(976);
+
+    return (
+        <>
+            {!isMobile ? (
+                <Card>
+                    {qrConfig.data == '' ? (
+                        <QREmptyState />
+                    ) : (
+                        <>
+                            <CardHeader>
+                                <CardTitle className="text-foreground text-xl">Preview</CardTitle>
+                            </CardHeader>
+                            <CardContent className="space-y-6">
+                                <QRDisplay ref={qrRef} config={qrConfig} />
+                                <div className="text-sm text-foreground text-center">
+                                    <p>Scan your QR Code to test it</p>
+                                </div>
+                            </CardContent>
+                            <CardFooter className="gap-3 w-full justify-center">
+                                <Button
+                                    variant="neon"
+                                    className="w-full sm:w-40 "
+                                    onClick={() => handleDownload(qrRef)}
+                                >
+                                    <DownloadIcon size={4} />
+                                    Download
+                                </Button>
+                                <Button
+                                    className="sm:w-40"
+                                    onClick={() => handleSaveQRCode()}
+                                    disabled={isSaving}
+                                >
+                                    <HeartPlus size={4} />
+                                    {!isMobile && (isSaving ? 'Saving...' : 'Save')}
+                                </Button>
+                            </CardFooter>
+                        </>
+                    )}
+                </Card>
+            ) : (
+                <QRPreviewModal
+                    qrRef={qrRef}
+                    onSave={handleSaveQRCode}
+                    onDownload={handleDownload}
+                    qrConfig={qrConfig}
+                    title={title}
+                />
+            )}
+        </>
+    );
+};
+
+export default memo(QRPreview);
