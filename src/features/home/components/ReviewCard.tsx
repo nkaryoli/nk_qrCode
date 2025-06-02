bfec2e0d7f2a5f7c4219873a2d2f3944998e51f4
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-02 17:36:12 +0200

    feat: update styles reviews card

--- src/features/home/components/ReviewCard.tsx
+++ src/features/home/components/ReviewCard.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Star } from "lucide-react";
 
 interface ReviewCardProps {
@@ -8,17 +9,21 @@ interface ReviewCardProps {
 
 const ReviewCard: React.FC<ReviewCardProps> = ({ user, text, rating = 5 }) => {
     return (
-        <div className="w-[300px] md:[400px] lg:w-full lg:max-w-md h-full min-h-56 bg-white shadow-lg p-5 rounded-lg border border-primary-300 shadow-primary-300">
-            <div className="flex flex-col lg:flex-row lg:items-center justify-between px-4 py-2 border-b">
-                <h3 className="text-primary-100 text-lg pb-1 lg:pb-0">{user}</h3>
-                <div className="flex space-x-2">
-                    {Array.from({ length: rating }).map((_, i) => (
-                        <Star key={i} className="text-yellow-400 w-5 h-5" fill="currentColor" />
-                    ))}
+        <Card className="w-[300px] md:[400px] lg:w-full lg:max-w-md h-full min-h-56  shadow-lg p-5 rounded-lg border border-primary-300 shadow-primary-300">
+            <CardHeader>
+                <div className="flex flex-col lg:flex-row lg:items-center justify-between py-2 border-b">
+                    <h3 className="text-primary text-lg pb-1 lg:pb-0">{user}</h3>
+                    <div className="flex space-x-2">
+                        {Array.from({ length: rating }).map((_, i) => (
+                            <Star key={i} className="text-secondary w-5 h-5" fill="currentColor" />
+                        ))}
+                    </div>
                 </div>
-            </div>
-            <p className="text-gray-700 p-4">{text}</p>
-        </div>
+            </CardHeader>
+            <CardContent>
+                <p>{text}</p>
+            </CardContent>
+        </Card>
     );
 };
 
