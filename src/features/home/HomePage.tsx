1fc4299a09f9e85b39c23639ac61083b70086c7e
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-07 22:16:25 +0200

    feat: update homepage

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,5 +1,5 @@
 import HeroSection from "./components/HeroSection"
-import HowItWorks from "./components/HowItWorks";
+
 import WhyNK from "./components/WhyNKCode";
 import ReviewCarousel from "./components/ReviewCarousel";
 import QRReader from "./components/qrReader/QRReader";
@@ -7,11 +7,8 @@ import QRReader from "./components/qrReader/QRReader";
 const HomePage = () => {
 	return (
 		<div className="w-full space-y-40">
-
 			<HeroSection />
-
 			<WhyNK/>
-			<HowItWorks />
 			<ReviewCarousel/>
 			<div id="qr-reader" className="w-full m-auto bg-gradient-to-br from-input via via-chart-5 to-input py-20">
 				<QRReader />
