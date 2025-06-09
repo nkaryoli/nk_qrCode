e853c2123980ca80b5751b06a353168576c3ba8c
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-09 21:34:39 +0200

    feat: update StepsCard

--- src/features/home/components/StepsCard.tsx
+++ src/features/home/components/StepsCard.tsx
@@ -16,30 +16,26 @@ interface StepsCardProps {
 const StepsCard: React.FC<StepsCardProps> = ({ number, img, text }) => {
     return (
         <Card
-            className="rounded-3xl bg-gradient-to-br from-chart-5 via-white to-chart-5 border-none shadow-lg
+            className="rounded-3xl bg-transparent border-none shadow-none
 				relative transition-all duration-500 ease-in overflow-hidden group 
-                hover:overflow-visible hover:shadow-lg hover:shadow-chart-5 hover:scale-105 hover:border-primary
+                hover:overflow-visible 
             "
         >
-            <div className="bg-white/60 h-full w-full p-6 rounded-3xl">
-                <CardHeader className="flex items-center text-center p-0">
-                    <CardTitle>
-                        <span
-                            className="text-chart-5 text-4xl font-bold absolute top-0 right-0 pl-3 pb-3 bg-white opacity-70 pr-1
-								flex items-center justify-center rounded-tr-3xl rounded-bl-3xl transition-all duration-500 ease-out
-								group-hover:text-5xl group-hover:-right-0 group-hover:opacity-100 group-hover:delay-100 
-								group-hover:py-3 group-hover:px-5 group-hover:border border-chart-5
+            <CardHeader className="flex items-center text-center p-0">
+                <CardTitle>
+                    <span
+                        className="text-gray-600 text-lg font-bold border border-white px-6
+                                flex items-center justify-center rounded-full
 							"
-                        >
-                            {number}
-                        </span>
-                    </CardTitle>
-                    <CardDescription>{img}</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p className="text-center text-lg text-balance py-3">{text}</p>
-                </CardContent>
-            </div>
+                    >
+                        Step {number}
+                    </span>
+                </CardTitle>
+                <CardDescription>{img}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <p className="text-center text-md text-balance py-3">{text}</p>
+            </CardContent>
         </Card>
     );
 };
