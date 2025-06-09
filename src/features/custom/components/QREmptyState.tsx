333309ec72a2add3d10afb5af20042e2c1be9f47
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-09 21:18:28 +0200

    feat: adjust styles QREmptyState component

--- src/features/custom/components/QREmptyState.tsx
+++ src/features/custom/components/QREmptyState.tsx
@@ -2,11 +2,9 @@ import { PacmanLoader } from 'react-spinners';
 
 export default function QREmptyState() {
     return (
-        <div className="flex flex-col items-center justify-center p-7.5 pb-9 text-center h-64 gap-9">
-            <div className="bg-radial from-card-foreground/20 via-card to-transparent ml-14 pr-24 rounded-full overflow-hidden">
-                <PacmanLoader size={25} color="#FF5E1F" className="-ml-6" speedMultiplier={0.5} />
-            </div>
-            <p className="text-foreground max-w-xs">
+        <div className="flex flex-col items-center justify-center p-7.5 text-center h-64 gap-9">
+            <PacmanLoader size={25} color="#FF5E1F" className="-ml-6" speedMultiplier={0.5} />
+            <p className="text-foreground text-balance">
                 This QR is hungry for content! Feed it with your ideas.
             </p>
         </div>
