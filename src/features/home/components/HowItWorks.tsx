d2304194076cfb75db9207bb5f6a681162cfb69e
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-13 18:33:24 +0200

    feat: add how it works updated

--- src/features/home/components/HowItWorks.tsx
+++ src/features/home/components/HowItWorks.tsx
@@ -2,25 +2,23 @@ import StepsCard from './StepsCard';
 
 const HowItWorks = () => {
     return (
-        <section className="w-full flex flex-col justify-center items-center py-24 backdrop-blur-sm bg-white space-y-4">
-            <h2 className="text-3xl md:text-5xl font-semibold">How It Works</h2>
-            <p className="text-lg max-w-[800px] mx-auto tracking-wid">
-                Get started in minutes with three simple steps process.
-            </p>
-            <div className="w-full max-w-5xl grid grid-cols-1 lg:grid-cols-3 gap-14 m-auto pt-9">
+        <section className="w-full max-w-4xl flex flex-col justify-center items-center space-y-9 m-auto">
+            <h2 className="text-2xl font-medium">How It Works</h2>
+
+            <div className="w-full flex justify-around ">
                 <StepsCard
                     number={1}
-                    img={<img src="/step1.svg" className="w-64" />}
+                    img={<img src="/step1.svg" className="w-40 mt-4 m-auto" />}
                     text="Type the content you want as a QR-Code."
                 />
                 <StepsCard
                     number={2}
-                    img={<img src="/step2.svg" className="w-60 mt-4" />}
+                    img={<img src="/step2.svg" className="w-44 mt-2 m-auto" />}
                     text="Customize it to your liking."
                 />
                 <StepsCard
                     number={3}
-                    img={<img src="/step3.svg" className="w-60 mt-4" />}
+                    img={<img src="/step3.svg" className="w-44 mt-2 m-auto" />}
                     text="Download it or Save it for future use."
                 />
             </div>
