beae13698a09c2893ca121b8cdc0a545e064f03f
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-13 18:34:33 +0200

    feat: change font size

--- src/features/home/components/heroSection/HeroSection.tsx
+++ src/features/home/components/heroSection/HeroSection.tsx
@@ -2,20 +2,20 @@ import QRGenerator from './QRGenerator';
 
 const Hero = () => {
     return (
-        <section className="flex flex-col items-center justify-center pt-32  overflow-hidden">
-            <div className="space-y-6 text-center">
-                <h1 className="text-4xl lg:text-5xl text-balance font-bold">
-                    Unleash Your Creativity with <br/>
-                    <span className="bg-gradient-to-r from-accent via-secondary to-primary bg-clip-text text-transparent leading-[3.5rem]">
+        <section className="flex flex-col items-center justify-center pt-32 overflow-hidden">
+            <div className="space-y-6 text-center max-w-5xl">
+                <h1 className="text-5xl max-w-4xl text-balance font-semibold">
+                    Unleash Your Creativity with {' '}
+                    <span className="bg-gradient-to-r from-accent via-secondary to-primary bg-clip-text text-transparent">
                         NK QR-Code
                     </span>
                 </h1>
-                <p className="w-full text-xl text-balance">
+                <p className="w-full text-lg text-balance md:px-9">
                     Generate, Customize and download your QR code in
                     <strong> three simple Steps</strong>
                 </p>
             </div>
-            <div className='bg_elipse relative w-full flex justify-center pt-16'>
+            <div className='bg_elipse relative w-full flex justify-center pt-16 pb-20'>
                 <div className='blur-bg'></div>
                 <div className='bg_lines'></div>
                 <div className='bg_lines2'></div>
