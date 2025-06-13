c53d03a3a5ee26fe8e49b5340e6a78b5522770c4
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-13 18:34:50 +0200

    feat: add sections to home page

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,11 +1,39 @@
 import { QRProvider } from '@/hooks/QRContext';
 import Hero from './components/heroSection/HeroSection';
+import Description from './components/Description';
+import ExploreSection from './components/ExploreSection';
+import AccountSection from './components/AccountSection';
+import HowItWorks from './components/HowItWorks';
+import CustomizeSection from './components/CustomizeSection';
+import TestimonialsSection from './components/TestimonialsSection';
+import QRReader from './components/qrReader/QRReader';
 
 const HomePage = () => {
     return (
         <QRProvider>
-            <div className="w-full">
+            <div className="w-full px-6 bg_gradient_white">
                 <Hero />
+            </div>
+            <div className="w-full bg-purple-100 py-20 px-6 md:px-16 ">
+                <Description />
+            </div>
+            <div className='py-24 px-6 md:px-16'>
+                <CustomizeSection/>
+            </div>
+            <div className='py-11 px-6 md:px-16'>
+                <ExploreSection/>
+            </div>
+            <div className='w-full bg-gradient-to-br from-input via-muted to-transparent px-6 md:px-16'>
+                <AccountSection/>
+            </div>
+            <div className='w-full p-6 md:p-16'>
+                <QRReader />
+            </div>
+            <div className='w-full px-6 md:px-16'>
+                <HowItWorks/>
+            </div>
+            <div className='py-24 px-6 md:px-16 w-full'>
+                <TestimonialsSection/>
             </div>
         </QRProvider>
     );
