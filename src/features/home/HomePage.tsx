02b784bf3ea09ec91bf9cb0032e836f3d1bb0c1d
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-14 19:42:15 +0200

    feat: adjust layout of home page

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -14,26 +14,16 @@ const HomePage = () => {
             <div className="w-full px-6 bg_gradient_white">
                 <Hero />
             </div>
-            <div className="w-full bg-purple-100 py-20 px-6 md:px-16 ">
-                <Description />
-            </div>
-            <div className='py-24 px-6 md:px-16'>
-                <CustomizeSection/>
-            </div>
-            <div className='py-11 px-6 md:px-16'>
-                <ExploreSection/>
-            </div>
-            <div className='w-full bg-gradient-to-br from-input via-muted to-transparent px-6 md:px-16'>
-                <AccountSection/>
-            </div>
-            <div className='w-full p-6 md:p-16'>
-                <QRReader />
-            </div>
-            <div className='w-full px-6 md:px-16'>
-                <HowItWorks/>
-            </div>
-            <div className='py-24 px-6 md:px-16 w-full'>
-                <TestimonialsSection/>
+            <Description />
+            <CustomizeSection />
+            <ExploreSection />
+            <AccountSection />
+            <QRReader />
+            <HowItWorks />
+            <div className="w-full h-72 relative bg-purple-300 mb-40">
+                <div className="absolute top-0 w-full">
+                    <TestimonialsSection />
+                </div>
             </div>
         </QRProvider>
     );
