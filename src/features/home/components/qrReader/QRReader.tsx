95c61fa3be4522dc3627224812c63450b3c8e50f
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-14 19:41:54 +0200

    feat. adjust layout

--- src/features/home/components/qrReader/QRReader.tsx
+++ src/features/home/components/qrReader/QRReader.tsx
@@ -2,8 +2,10 @@ import QrScannerModal from '@/features/home/components/qrReader/QrScannerModal'
 
 const QRReader = () => {
 	return (
-		<section className="w-full max-w-4xl m-auto flex flex-col lg:flex-row md:justify-between md:items-center gap-10 px-9">
-			<div className="max-w-md space-y-6 drop-shadow-[0_0_3px_var(--background)]" >
+		<section className="w-full p-9 lg:p-20">
+			<div className='p-9 max-w-4xl m-auto flex flex-col-reverse lg:flex-row md:justify-center items-center gap-6  rounded-xl shadow-[0px_0px_5px_var(--primary)]'>
+				
+			<div className="max-w-lg text-balance space-y-6 text-center lg:text-start" >
 				<p className="mb-3">Scan</p>
 				<h1 className="text-2xl font-medium">
 					Scan & Discover with our <span className='text-primary'>QR Reader!!</span>
@@ -14,8 +16,9 @@ const QRReader = () => {
 				</p>
 				<QrScannerModal />
 			</div>
-			<div className='p-9 pr-11 bg-card w-fit rounded-ss-[150px] rounded-ee-[150px] rounded-3xl'>
-				<img src='/qrReaderImg.svg' className='w-72'/>
+			<div className='py-6 px-9 lg:bg-white border border-primary w-fit rounded-ss-[120px] rounded-ee-[120px] shadow-[15px_15px_0px_var(--primary)]'>
+				<img src='/qrReaderImg.svg' className='w-28 lg:w-40'/>
+			</div>
 			</div>
 		</section>
 	)
