3b391ef0adebe189c20831913f28412dc1838a53
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-14 19:41:37 +0200

    feat: change styles of steps card

--- src/features/home/components/StepsCard.tsx
+++ src/features/home/components/StepsCard.tsx
@@ -1,10 +1,3 @@
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from '@/components/ui/card';
 import type { ReactNode } from 'react';
 
 interface StepsCardProps {
@@ -15,23 +8,22 @@ interface StepsCardProps {
 
 const StepsCard: React.FC<StepsCardProps> = ({ number, img, text }) => {
     return (
-        <Card className="bg-gradient-to-br from-purple-200 via-white to-purple-200 border-none  max-w-56 w-full" >
-            <CardHeader className="flex items-center text-center pb-0">
-                <CardTitle>
-                    <span
-                        className="text-lg bg-white px-6
-                                flex items-center justify-center rounded-full
-							"
-                    >
-                        Step {number}
-                    </span>
-                </CardTitle>
-            </CardHeader>
-            <CardContent>
-                <CardDescription>{img}</CardDescription>
-                <p className="text-center text-md text-balance py-3">{text}</p>
-            </CardContent>
-        </Card>
+        <div
+            className="w-full max-w-lg h-36 items-center flex p-3 rounded-lg
+                bg-gradient-to-br from-white via-white to-white border border-muted 
+                hover:from-purple-300/20 hover:via-white hover:to-white hover:border hover:border-chart-5
+            "
+        >
+            <div className=''>{img}</div>
+            <div className='flex flex-col p-3 gap-3'>
+                <span
+                    className="font-medium bg-white rounded-full"
+                >
+                    Step {number}
+                </span>
+            <p>{text}</p>
+            </div>
+        </div>
     );
 };
 
