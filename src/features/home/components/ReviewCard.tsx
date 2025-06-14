132508547a6c574adf90e900becfd4714f656769
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-14 19:38:40 +0200

    feat: add animation to carousel

--- src/features/home/components/ReviewCard.tsx
+++ src/features/home/components/ReviewCard.tsx
@@ -5,15 +5,18 @@ interface ReviewCardProps {
     user: string;
     text: string;
     rating?: number;
+    isActive?: boolean;
 }
 
-const ReviewCard: React.FC<ReviewCardProps> = ({ user, text, rating = 5 }) => {
+const ReviewCard: React.FC<ReviewCardProps> = ({ user, text, rating = 5, isActive }) => {
     return (
-        <Card className="h-full">
+        <Card className={`h-full border-none transition-all duration-300 ${
+            isActive ? "bg-purple-900 text-white" : "bg-white tex-black"
+        }`}>
             <CardHeader>
-                <div className="flex justify-between py-2 border-b">
+                <div className="flex justify-between py-2 border-b ">
                     <h3 className="text-lg pb-1 lg:pb-0">{user}</h3>
-                    <div className="flex space-x-1">
+                    <div className="flex space-x-1 ">
                         {Array.from({ length: rating }).map((_, i) => (
                             <Star key={i} size={18} className="text-secondary" fill="currentColor" />
                         ))}
@@ -21,7 +24,7 @@ const ReviewCard: React.FC<ReviewCardProps> = ({ user, text, rating = 5 }) => {
                 </div>
             </CardHeader>
             <CardContent>
-                <p>{text}</p>
+                <p className="">{text}</p>
             </CardContent>
         </Card>
     );
--- src/features/home/components/TestimonialsSection.tsx
+++ src/features/home/components/TestimonialsSection.tsx
@@ -2,41 +2,91 @@ import {
     Carousel,
     CarouselContent,
     CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
+    type CarouselApi,
 } from '@/components/ui/carousel';
 import { reviews } from '@/constants';
 import ReviewCard from './ReviewCard';
+import React, { useEffect } from 'react';
+import { useIsMobile } from '@/hooks/useIsMobile';
 
 const TestimonialsSection = () => {
+    const [api, setApi] = React.useState<CarouselApi | null>(null);
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+    const isTablet = useIsMobile(1300);
+    const isMobile = useIsMobile(678);
+
+    useEffect(() => {
+        if (!api) return;
+        const interval = setInterval(() => {
+            if (!api) return;
+            if (api.selectedScrollSnap() === api.scrollSnapList().length - 1) {
+                api.scrollTo(0);
+            } else {
+                api.scrollNext();
+            }
+        }, 5500);
+        return () => clearInterval(interval);
+    }, [api]);
+
+    useEffect(() => {
+        if (!api) return;
+        if (isMobile){
+            setSelectedIndex((api.selectedScrollSnap()));
+            api.on('select', () => {
+                setSelectedIndex((api.selectedScrollSnap()));
+            });
+        } else if (isTablet){
+            setSelectedIndex((api.selectedScrollSnap()) + 1);
+            api.on('select', () => {
+                setSelectedIndex((api.selectedScrollSnap() + 1));
+            });
+        } else {
+            setSelectedIndex((api.selectedScrollSnap()) + 2);
+            api.on('select', () => {
+                setSelectedIndex((api.selectedScrollSnap()) + 2);
+            });
+        }
+    }, [api, isMobile, isTablet]);
+
     return (
-        <div className='w-full max-w-4xl space-y-4 m-auto'>
-			<h2 className='text-2xl font-medium text-center'>NK QR-Code Reviews</h2>
-			<p className='text-center pb-3'>Discover what our customers say about their encounters with our versatile and user-friendly platform.</p>
-            <Carousel
-                opts={{
-                    align: 'start',
-                }}
-                className="w-[80%] m-auto md:w-full"
-            >
-                <CarouselContent>
-                    {reviews.map((review, index) => (
-                        <CarouselItem
-                            key={index}
-                            className="md:basis-1/2 lg:basis-1/3 pb-1"
-                        >
-                            <ReviewCard
-                                user={review.user}
-                                text={review.text}
-                                rating={review.rating}
-                            />
-                        </CarouselItem>
-                    ))}
-                </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
-            </Carousel>
-        </div>
+        <section className="w-full py-9 lg:py-20">
+            <div className="w-full space-y-4 m-auto">
+                <h2 className="text-2xl font-medium text-center">
+                    NK QR-Code Reviews
+                </h2>
+                <p className="text-center pb-3">
+                    Discover what our customers say about their encounters with
+                    our versatile and user-friendly platform.
+                </p>
+                <Carousel
+                    setApi={setApi}
+                    opts={{
+                        align: 'start',
+                    }}
+                    className="m-auto md:w-full"
+                >
+                    <CarouselContent>
+                        {reviews.map((review, index) => (
+                            <CarouselItem
+                                key={index}
+                                className={`md:basis-1/3 xl:basis-1/5 py-2 transition-all duration-300 ${
+                                    selectedIndex === index
+                                        ? 'scale-105 z-10'
+                                        : 'scale-95'
+                                }`}
+                            >
+                                <ReviewCard
+                                    user={review.user}
+                                    text={review.text}
+                                    rating={review.rating}
+                                    isActive={selectedIndex === index}
+                                />
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                </Carousel>
+            </div>
+        </section>
     );
 };
 
