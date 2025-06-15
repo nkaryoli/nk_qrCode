263fac7f7e5e21d55269a9fc87a19b26a7c55c52
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-15 22:09:31 +0200

    feat: adjust card styles

--- src/features/home/components/howItWorks/StepsCard.tsx
+++ src/features/home/components/howItWorks/StepsCard.tsx
@@ -10,14 +10,14 @@ const StepsCard: React.FC<StepsCardProps> = ({ number, img, text }) => {
     return (
         <div
             className="w-full max-w-lg h-36 items-center flex p-3 rounded-lg
-                bg-gradient-to-br from-white via-white to-white border border-muted 
-                hover:from-purple-300/20 hover:via-white hover:to-white hover:border hover:border-chart-5
+                bg-gradient-to-br from-white/40 via-white/30 to-white/10 border border-purple-100 
+                hover:from-white/40 hover:via-white/80 hover:to-white/70 hover:border hover:border-purple-300 hover:drop-shadow-[1px_1px_2px_var(--chart-5)]
             "
         >
             <div className=''>{img}</div>
             <div className='flex flex-col p-3 gap-3'>
                 <span
-                    className="font-medium bg-white rounded-full"
+                    className="text-lg font-medium text-purple-950 rounded-full"
                 >
                     Step {number}
                 </span>
