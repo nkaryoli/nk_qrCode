7f2f286bf08e474866a6ada8cd9105928c9faa7f
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-16 10:12:35 +0200

    feat: adjust padding y

--- src/features/home/components/CTASection.tsx
+++ src/features/home/components/CTASection.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button';
 
 const CTASection = () => {
     return (
-        <section className="w-full py-24 px-9 lg:p-20 mt-20 bg-gradient-to-b from-input via-input to-transparent">
+        <section className="w-full py-16 lg:py-20 px-9 lg:p-20 mt-20 bg-gradient-to-b from-input via-input to-transparent">
             <div className="max-w-4xl flex flex-col items-center lg:flex-row gap-9 m-auto rounded-lg">
                 <div className="space-y-6 w-full p-3 text-center lg:text-start">
                     <h1 className="text-2xl font-medium">
