808e20ba6be1513772b7a93e8c33a20562d701ff
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-16 19:59:48 +0200

    feat: add funtionality to button  on CTASection

--- src/features/home/components/CTASection.tsx
+++ src/features/home/components/CTASection.tsx
@@ -1,6 +1,13 @@
 import { Button } from '@/components/ui/button';
+import { QrCode } from 'lucide-react';
+import { HashLink } from 'react-router-hash-link';
 
-const CTASection = () => {
+interface CTASectionProps {
+    handleSigIn: () => void;
+    isSignedIn: boolean | undefined;
+}
+
+const CTASection: React.FC<CTASectionProps> = ({ handleSigIn, isSignedIn }) => {
     return (
         <section className="w-full py-16 lg:py-20 px-9 lg:p-20 mt-20 bg-gradient-to-b from-input via-input to-transparent">
             <div className="max-w-4xl flex flex-col items-center lg:flex-row gap-9 m-auto rounded-lg">
@@ -8,17 +15,33 @@ const CTASection = () => {
                     <h1 className="text-2xl font-medium">
                         Create your QR Code today!
                     </h1>
-                    <p className="text-balance">
+                    <p className="max-w-lg m-auto lg:m-0">
                         Experience the benefits of our service now. Join us now
                         to design and customize your own QR codes effortlessly
                         and efficiently.
                     </p>
-                    <Button>Create QR</Button>
-                    <Button variant={'neon'} className="ml-3 bg-transparent">
-                        Sign Up
-                    </Button>
+                    <div className="flex w-full gap-3 justify-center lg:justify-start">
+                        <Button className="w-40" asChild>
+                            <HashLink
+                                smooth
+                                to={isSignedIn ? '/dashboard' : '/#home'}
+                            >
+                                <QrCode />
+                                Create QR
+                            </HashLink>
+                        </Button>
+                        {!isSignedIn && (
+                            <Button
+                                variant={'outline'}
+                                className="w-40"
+                                onClick={handleSigIn}
+                            >
+                                Sign Up
+                            </Button>
+                        )}
+                    </div>
                 </div>
-                <img src="/qrCode.png" alt="" className="rounded-xl" />
+                <img src="/qrCode.png" alt="" className="rounded-xl w-44" />
             </div>
         </section>
     );
