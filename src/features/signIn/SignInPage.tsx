c4844973d00ec7875fd180ad0d6bacd922367363
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-17 13:36:23 +0200

    feat: change SignIn page styles

--- src/features/signIn/SignInPage.tsx
+++ src/features/signIn/SignInPage.tsx
@@ -5,45 +5,37 @@ import { CircleLoader } from 'react-spinners';
 
 export function SignInPage() {
     return (
-        <section className="w-full min-h-screen flex items-center justify-center bg-background py-24 px-3">
-            <div className="flex flex-col lg:flex-row w-full max-w-xl lg:max-w-6xl mx-auto lg:shadow-xl rounded-xl lg:overflow-hidden">
-                <div className="w-full lg:max-w-[55%] flex flex-col items-center lg:items-start lg:pt-24 py-11 p-3 lg:p-16 relative">
-                    <div className="w-full text-center">
-                        <h1 className="text-4xl md:text-5xl font-bold mb-6 text-primary">
-                            Welcome Back!
-                        </h1>
-                        <p>We are very excited to see you.</p>
-                        <p>
-                            Please Sign into your{' '}
-                            <strong className="text-primary text-lg ">
-                                NK-QRcode
-                            </strong>{' '}
-                            account
-                        </p>
-                    </div>
-                    <div className="relative w-fit m-auto mt-6">
-                        <img
-                            src="/qrCode.png"
-                            className="rounded-xl w-[25%] mt-6 block m-auto absolute left-[55%]"
-                        />
-                        <img src="/abstract.svg" className="w-[400px]" />
-                    </div>
+        <section className="w-full flex items-center justify-center bg-background pt-32 px-3">
+            <div className="flex flex-col gap-6 lg:gap-0 lg:flex-row w-full max-w-xl lg:max-w-4xl mx-auto lg:shadow-xl rounded-xl lg:overflow-hidden">
+                <div className="w-full lg:max-w-[55%] flex flex-col items-center lg:p-16 relative">
+                    <h1 className="text-4xl font-medium mb-6 text-purple-950">
+                        Welcome Back!
+                    </h1>
+                    <p className="mt-6">We are very excited to see you.</p>
+                    <p>
+                        Please Sign into your{' '}
+                        <strong className="text-primary font-header text-lg ">
+                            NK-QRcode
+                        </strong>{' '}
+                        account.
+                    </p>
+                    <img src="/qrCode.png" className="m-auto hidden lg:block" />
                     <QrCode
                         size={'35%'}
-                        className="-rotate-45 absolute -left-20 -top-11 text-chart-5"
+                        className="hidden lg:block -rotate-45 absolute -left-20 -top-11 text-purple-500  rounded-lg"
                     />
                 </div>
 
-                <div className="flex items-center justify-center w-full lg:w-[45%] lg:min-w-[400px] lg:bg-chart-5 lg:px-24 relative overflow-hidden">
+                <div className="flex items-center justify-center w-full lg:w-[45%] lg:min-w-[400px] lg:bg-gradient-to-b from-purple-950 to-purple-600 lg:px-24 relative overflow-hidden">
                     <ClerkLoading>
                         <CircleLoader className="" color="#ffffff" />
                     </ClerkLoading>
                     <QrCode
                         size={'32%'}
-                        className="-rotate-12 absolute -right-14 -bottom-14 text-white/50"
+                        className="hidden lg:block -rotate-12 absolute -right-14 -bottom-14 text-purple-300"
                     />
                     <motion.div
-                        className="w-fit h-[537px] lg:h-fit bg-chart-5 rounded-xl"
+                        className="w-fit h-[637px] sm:h-[550px] lg:h-fit bg-gradient-to-b from-purple-950 to-purple-600 lg:from-purple-950/0 lg:to-purple-800/0 rounded-xl pt-7 pb-9"
                         initial={{ opacity: 0, x: -20 }}
                         animate={{ opacity: 1, x: 0 }}
                         transition={{
@@ -66,10 +58,9 @@ export function SignInPage() {
                                 elements: {
                                     cardBox: 'shadow-none w-[100]',
                                     formButtonPrimary: { height: '40px' },
-                                    socialButtonsIconButton:
-                                        'bg-white/10 border-primary',
+                                    socialButtonsIconButton: 'bg-white/20',
                                     headerTitle:
-                                        "text-[0px] before:content-['Sign_in'] before:text-4xl before:text-white font-header",
+                                        "text-[0px] before:content-['Sign_in'] before:text-3xl before:text-white before:font-medium font-header",
                                     headerSubtitle: 'hidden',
                                     footer: 'bg-none',
                                     card: {
