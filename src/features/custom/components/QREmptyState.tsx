9875197d6c085d8b6b04a47e33f8c43e9ede3954
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-18 10:49:26 +0200

    feat: change color of emptyState loader

--- src/features/custom/components/QREmptyState.tsx
+++ src/features/custom/components/QREmptyState.tsx
@@ -3,7 +3,7 @@ import { PacmanLoader } from 'react-spinners';
 export default function QREmptyState() {
     return (
         <div className="flex flex-col items-center justify-center p-7.5 text-center h-64 gap-9">
-            <PacmanLoader size={25} color="#FF5E1F" className="-ml-6" speedMultiplier={0.5} />
+            <PacmanLoader size={25} color="#3b0764" className="-ml-6" speedMultiplier={0.5} />
             <p className="text-foreground text-balance">
                 This QR is hungry for content! Feed it with your ideas.
             </p>
