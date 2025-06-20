c5a5d32e4eec026ce520eca9daf760faf42d5f23
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-20 17:43:10 +0200

    feat: add items to user button form clerk

--- src/components/layout/header/ActionButtons.tsx
+++ src/components/layout/header/ActionButtons.tsx
@@ -9,6 +9,7 @@ import {
     UserButton,
 } from '@clerk/clerk-react';
 import { motion } from 'framer-motion';
+import { CirclePlus, QrCode, Settings, SwatchBook } from 'lucide-react';
 
 interface ActionButtonsProps {
     handleLogIn: () => void;
@@ -69,7 +70,55 @@ const ActionButtons: React.FC<ActionButtonsProps> = ({
                     }}
                     className="h-8 w-8 ml-16 mr-3 flex justify-end rounded-full bg-gradient-to-br from-primary via-accent to-secondary p-0.5"
                 >
-                    <UserButton />
+                    <UserButton
+                        appearance={{
+                            elements: {
+                                userButtonPopoverFooter: 'hidden',
+                                userButtonPopoverActionButton__manageAccount:
+                                    'hidden',
+                                userButtonPopoverCard:
+                                    'w-72 !bg-gradient-to-b !from-transparent !via-purple-50 !to-white rounded-b-lg rounded-t-[0px] mt-2.5 shadow-md',
+                                userPreviewMainIdentifierText__userButton:
+                                    'text-primary text-[16px] font-bold',
+                                userButtonPopoverMain:
+                                    'bg-transparent backdrop-blur-md rounded-t-[0px]',
+                                userButtonPopoverActions: '',
+                                userButtonPopoverActionButton:
+                                    'hover:text-primary font-medium text-[16px] gap-0',
+                                userButtonPopoverCustomItemButton:
+                                    'hover:text-primary font-medium text-[16px] !border-none gap-0',
+                            },
+                        }}
+                    >
+                        <UserButton.MenuItems>
+                            <UserButton.Link
+                                label="New QR"
+                                labelIcon={<CirclePlus size={16} />}
+                                href="/dashboard"
+                            />
+                        </UserButton.MenuItems>
+                        <UserButton.MenuItems>
+                            <UserButton.Link
+                                label="My QR Codes"
+                                labelIcon={<QrCode size={16} />}
+                                href="/dashboard"
+                            />
+                        </UserButton.MenuItems>
+                        <UserButton.MenuItems>
+                            <UserButton.Link
+                                label="My Templates"
+                                labelIcon={<SwatchBook size={16} />}
+                                href="/dashboard"
+                            />
+                        </UserButton.MenuItems>
+                        <UserButton.MenuItems>
+                            <UserButton.Link
+                                label="Settings"
+                                labelIcon={<Settings size={16} />}
+                                href="/dashboard"
+                            />
+                        </UserButton.MenuItems>
+                    </UserButton>
                 </motion.div>
             </SignedIn>
             <motion.div
