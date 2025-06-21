6f018a6797a1cb725a7d8ff42837a964e122f006
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-21 21:52:40 +0200

    feat: add CustomizeQR component

---
+++ src/features/dashboard/components/newQR/CustomizeQR.tsx
@@ -0,0 +1,45 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import QRConfigForm from '@/features/custom/components/QRConfigForm/QRConfigForm';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
+
+interface CustomizeProps {
+    setActive: (active: string) => void;
+}
+
+const CustomizeQR: React.FC<CustomizeProps> = ({ setActive }) => {
+    return (
+        <div className="w-full h-full flex flex-col gap-6">
+            <Card className="border-none lg:pt-4 lg:pb-6 lg:px-3">
+                <CardHeader>
+                    <h2 className="text-lg font-medium text-purple-950">
+                        Customize your QR Code
+                    </h2>
+                </CardHeader>
+                <CardContent>
+                    <QRConfigForm />
+                </CardContent>
+            </Card>
+            <div className="flex items-center justify-end gap-4">
+                <Button
+                    size={'sm'}
+                    variant={'outline'}
+                    onClick={() => setActive('content')}
+                    className='opacity-80'
+                >
+                    <ArrowLeft />
+                    Back
+                </Button>
+                <Button 
+                    size={'sm'} 
+                    onClick={() => setActive('download')}
+                >
+                    Next
+                    <ArrowRight />
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+export default CustomizeQR;
