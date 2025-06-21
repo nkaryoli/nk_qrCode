6573ed0c1f38647e8e6b7f4b3d70b8ec35fd4f6d
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-06-21 21:57:27 +0200

    feat: update styles of QREmptyState componenent

--- src/features/custom/components/QREmptyState.tsx
+++ src/features/custom/components/QREmptyState.tsx
@@ -2,8 +2,8 @@ import { PacmanLoader } from 'react-spinners';
 
 export default function QREmptyState() {
     return (
-        <div className="flex flex-col items-center justify-center p-7.5 text-center h-64 gap-9">
-            <PacmanLoader size={25} color="#3b0764" className="-ml-6" speedMultiplier={0.5} />
+        <div className="flex flex-col items-center justify-center p-7 text-center h-64 gap-9 w-[350px]">
+            <PacmanLoader size={25} color="#fc4700" className="-ml-6" speedMultiplier={0.5} />
             <p className="text-foreground text-balance">
                 This QR is hungry for content! Feed it with your ideas.
             </p>
