f1d656225560a2c999dff3b795427017b9205484
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-09-20 22:43:13 +0200

    feat: add fetching qrs and deleting fron database

--- src/api/qrApi.tsx
+++ src/api/qrApi.tsx
@@ -1,10 +1,7 @@
 import type { QRCode, QRCodeInput } from '@/supabase/types';
 import { supabase } from '@/supabase/supabaseClient';
 
-export const createQr = async (
-    input: QRCodeInput,
-
-): Promise<QRCode> => {
+export const createQr = async ( input: QRCodeInput ): Promise<QRCode> => {
 
     const { data, error } = await supabase.from('qrs').insert(input).select().single();
 
@@ -13,3 +10,26 @@ export const createQr = async (
 
     return data as QRCode;
 };
+
+export const getQRs = async (user_id: string): Promise<QRCode[]> => {
+    const { data, error } = await supabase.from('qrs').select('*').eq('user_id', user_id);   
+
+    if (error) throw new Error(`Error geting qrcodes: ${error.message}`);
+    if (!data) throw new Error('No QR code returned from select.');
+
+    return data as QRCode[];
+}
+
+export const deleteQR = async (qr_id: number): Promise<number | null> => {
+    const { data, error } = await supabase
+        .from('qrs')
+        .delete()
+        .eq('id', qr_id)
+        .select('id') // <-- selecciona el id borrado
+        .single();
+
+    if (error) throw new Error(`Error deleting qrcode: ${error.message}`);
+    if (!data) return null;
+
+    return data.id;
+}
