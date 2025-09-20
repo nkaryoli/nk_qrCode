d6b31c0c13edb6b0847ffda718ef8fff09f60d0d
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-09-20 22:44:21 +0200

    feat: add TemplateCard component for displaying QR code templates with delete functionality

--- src/features/dashboard/components/userQRs/MyQRs.tsx
+++ src/features/dashboard/components/userQRs/MyQRs.tsx
@@ -2,6 +2,7 @@ import type { QRCode } from '@/supabase/types';
 import { PacmanLoader } from 'react-spinners';
 import { Button } from '@/components/ui/button';
 import { QrCode } from 'lucide-react';
+import TemplateCard from './TemplateCard';
 
 interface MyQRsProps {
     qrs: QRCode[];
@@ -9,8 +10,11 @@ interface MyQRsProps {
     handleSidebarSelect: (id: string) => void;
 }
 
-const MyQRs: React.FC<MyQRsProps> = ({ qrs, isLoading = false, handleSidebarSelect }) => {
-
+const MyQRs: React.FC<MyQRsProps> = ({
+    qrs,
+    isLoading = false,
+    handleSidebarSelect,
+}) => {
     if (isLoading) {
         return (
             <div className="w-full h-[50vh] flex items-center justify-center">
@@ -43,14 +47,17 @@ const MyQRs: React.FC<MyQRsProps> = ({ qrs, isLoading = false, handleSidebarSele
     }
 
     return (
-        <div className="w-full flex flex-col items-center justify-center gap-9">
-            <>
-                <h2 className="text-2xl font-semibold w-full max-w-5xl">My QR Codes</h2>
-                
-            </>
+        <div className="w-full min-h-screen lg:p-9 flex flex-col items-center justify-center gap-9 bg-purple-50">
+            <h2 className="text-2xl font-semibold w-full max-w-5xl">
+                My QR Codes
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  gap-6 relative z-10 py-10 max-w-7xl mx-auto">
+                {qrs.map((qr) => (
+                    <TemplateCard key={qr.id} qrTemplate={qr.qr_template} qr_id={qr.id} />
+                ))}
+            </div>
         </div>
     );
 };
 
 export default MyQRs;
-
