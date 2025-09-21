030a0e998a1531b435b14e81ad92586eb5614892
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-09-21 22:55:24 +0200

    feat: implement sign-up functionality and enhance authentication context with Google and GitHub sign-in options

--- src/features/signIn/SignInPage.tsx
+++ src/features/signIn/SignInPage.tsx
@@ -2,16 +2,53 @@ import { motion } from 'framer-motion';
 import { QrCode } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { useAuth } from '@/hooks/AuthContext';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Mail, Lock } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function SignInPage() {
-    const { signInWhitGithub, signOut, user } = useAuth();
+    const { signUpNewUser, signInWithGoogle, signInWithGithub, signOut, user } =
+        useAuth();
+    const [email, setEmal] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [error, setError] = useState<string | null>(null);
+    const [isSigningUp, setIsSigningUp] = useState(false);
+    const navigate = useNavigate();
+
+
+    const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsSigningUp(true);
+
+        try {
+            const result = await signUpNewUser(email, password);
+            if (!result.success) {
+                console.log('User signed up successfully:', result.data);
+                setIsSigningUp(false);
+                return;
+            }
+        } catch (error) {
+            console.log('Error during sign up:', error);
+            setError('Failed to sign up. Please try again.');
+            setIsSigningUp(false);
+        }
+    };
+    useEffect(() => {
+        if (user && isSigningUp) {
+            navigate('/dashboard');
+            setIsSigningUp(false);
+        }
+    }, [user, isSigningUp, navigate]);
 
     const displayName = user?.user_metadata.user_name || user?.email;
 
     return (
         <section className="w-full flex items-center justify-center bg-background pt-32 px-3">
             <div className="flex flex-col gap-6 lg:gap-0 lg:flex-row w-full max-w-xl lg:max-w-4xl mx-auto lg:shadow-xl rounded-xl lg:overflow-hidden">
-                <div className="w-full lg:max-w-[55%] flex flex-col items-center lg:p-16 relative">
+                <div className="w-full lg:max-w-[50%] flex flex-col items-center lg:p-16 relative">
                     <h1 className="text-4xl font-medium mb-6 text-purple-950">
                         Welcome Back!
                     </h1>
@@ -30,13 +67,13 @@ export function SignInPage() {
                     />
                 </div>
 
-                <div className="flex items-center justify-center w-full lg:w-[45%] lg:min-w-[400px] lg:bg-gradient-to-b from-purple-950 to-purple-600 lg:px-24 relative overflow-hidden">
+                <div className="flex items-center justify-center w-full lg:w-[50%] lg:min-w-[400px] lg:bg-gradient-to-b from-purple-950 to-purple-600 lg:p-14 relative overflow-hidden">
                     <QrCode
                         size={'32%'}
                         className="hidden lg:block -rotate-12 absolute -right-14 -bottom-14 text-purple-300"
                     />
                     <motion.div
-                        className="w-fit h-[637px] sm:h-[550px] lg:h-fit bg-gradient-to-b from-purple-950 to-purple-600 lg:from-purple-950/0 lg:to-purple-800/0 rounded-xl pt-7 pb-9"
+                        className="w-full h-[637px] sm:h-[550px] lg:h-fit bg-gradient-to-b from-purple-950 to-purple-600 lg:from-purple-950/0 lg:to-purple-800/0 rounded-xl "
                         initial={{ opacity: 0, x: -20 }}
                         animate={{ opacity: 1, x: 0 }}
                         transition={{
@@ -60,9 +97,124 @@ export function SignInPage() {
                                 </Button>
                             </div>
                         ) : (
-                            <Button onClick={signInWhitGithub}>
-                                Sign in with github
-                            </Button>
+                            <div className='w-full flex flex-col gap-4'>
+                                <form
+                                    onSubmit={handleSignUp}
+                                    className="w-full flex flex-col gap-4"
+                                >
+                                    {/* <p className='text-white'>Please enter your email below to sign in</p> */}
+                                    {/* Email */}
+                                    <div className="flex flex-col gap-2">
+                                        <Label
+                                            htmlFor="email"
+                                            className="text-white"
+                                        >
+                                            Email
+                                        </Label>
+                                        <div className="flex items-center gap-2 border rounded-sm px-3 h-10 focus-within:ring-2 focus-within:ring-ring bg-white">
+                                            <Mail className="h-5 w-5 text-muted-foreground" />
+                                            <Input
+                                                id="email"
+                                                type="email"
+                                                placeholder="Enter your email"
+                                                className="border-0 shadow-none focus-visible:ring-0"
+                                                onChange={(e) =>
+                                                    setEmal(e.target.value)
+                                                }
+                                            />
+                                        </div>
+                                    </div>
+
+                                    {/* Password */}
+                                    <div className="flex flex-col gap-2">
+                                        <Label
+                                            htmlFor="password"
+                                            className="text-white"
+                                        >
+                                            Password
+                                        </Label>
+                                        <div className="flex items-center gap-2 bg-white border rounded-sm  px-3 h-10 focus-within:ring-2 focus-within:ring-ring">
+                                            <Lock className="h-5 w-5 text-muted-foreground" />
+                                            <Input
+                                                id="password"
+                                                type="password"
+                                                placeholder="Enter your password"
+                                                className="border-0 shadow-none focus-visible:ring-0"
+                                                onChange={(e) =>
+                                                    setPassword(e.target.value)
+                                                }
+                                            />
+                                        </div>
+                                    </div>
+
+                                    {/* Remember me & Forgot */}
+                                    <div className="flex items-center justify-between">
+                                        <div className="flex items-center space-x-2">
+                                            <Checkbox id="remember" />
+                                            <Label
+                                                htmlFor="remember"
+                                                className="text-sm font-normal text-white"
+                                            >
+                                                Remember me
+                                            </Label>
+                                        </div>
+                                        <button className="text-sm text-primary hover:underline ml-1">
+                                            Forgot password?
+                                        </button>
+                                    </div>
+
+                                    {/* Submit */}
+                                    <Button type="submit"
+                                    >
+                                        Sign In
+                                    </Button>
+                                    {error && <p className='text-red-600 text-center pt-4'>{error}</p>}
+                                </form>
+                                
+                                <hr />
+
+                                {/* Social login buttons */}
+                                <div className="flex flex-col gap-3">
+                                    <Button
+                                        onClick={signInWithGoogle}
+                                        variant="outline"
+                                        className="bg-gray-300"
+                                    >
+                                        <img
+                                            src="https://www.svgrepo.com/show/355037/google.svg"
+                                            alt="Google"
+                                            width={20}
+                                            height={20}
+                                        />
+                                        Continue with Google
+                                    </Button>
+
+                                    <button
+                                        onClick={signInWithGithub}
+                                        className="w-full h-10 rounded-md shadow-md bg-black text-white flex items-center justify-center gap-3
+                                            hover:bg-zinc-800 hover:scale-[103%] transition"
+                                    >
+                                        <img
+                                            src="/githubIcon.svg"
+                                            alt="GitHub"
+                                            width={20}
+                                            height={20}
+                                        />
+                                        Continue with GitHub
+                                    </button>
+                                </div>
+
+                                {/* Signup */}
+                                <p className="text-center text-sm text-white mt-2">
+                                    Don’t have an account?{' '}
+                                    <a
+                                        href="/sign-up"
+                                        className="text-primary cursor-pointer hover:underline"
+                                    >
+                                        Sign Up
+                                    </a>
+                                </p>
+                            </div>
                         )}
                     </motion.div>
                 </div>
--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -4,8 +4,16 @@ import { createContext, useContext, useEffect, useState } from 'react';
 
 interface AuthContextType {
     user: User | null;
-    signInWhitGithub: () => void;
+    signInWithGithub: () => void;
+    signInWithGoogle: () => void;
     signOut: () => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    signUpNewUser: (email: string, password: string) => Promise<{ success: boolean; data: any }>;
+    SignInUser: (
+        email: string,
+        password: string
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ) => Promise<{ success: boolean; data: any }>;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -13,30 +21,75 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const [user, setUser] = useState<User | null>(null);
 
-	useEffect(() => {
-		supabase.auth.getSession().then(({data: {session}}) => {
-			setUser(session?.user ?? null);
-		});
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setUser(session?.user ?? null);
+        });
 
-		const {data: listener} = supabase.auth.onAuthStateChange((_, session) => {
-			setUser(session?.user ?? null);
-		});
+        const { data: listener } = supabase.auth.onAuthStateChange(
+            (_, session) => {
+                setUser(session?.user ?? null);
+            }
+        );
 
-		return () => {
-			listener.subscription.unsubscribe();
-		}
-	}, [])
+        return () => {
+            listener.subscription.unsubscribe();
+        };
+    }, []);
 
-    const signInWhitGithub = () => {
+    const SignInUser = async (email: string, password: string) => {
+        try {
+            const { data, error } = await supabase.auth.signInWithPassword({
+                email: email,
+                password: password,
+            });
+            if (error) {
+                console.log('Error signing in:', error.message);
+                return { success: false, data: null };
+            }
+            console.log('Sign-in successful:', data);
+            return { success: true, data };
+        } catch (error) {
+            console.log('Unexpected error signing in:', error);
+            return { success: false, data: null };
+        }
+    };
+
+    const signUpNewUser = async (email: string, password: string) => {
+        const { data, error } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        });
+        if (error) {
+            console.log('Error signing up:', error.message);
+            return { success: false, data: null };
+        }
+        return { success: true, data };
+    };
+
+    const signInWithGithub = () => {
         supabase.auth.signInWithOAuth({ provider: 'github' });
     };
 
+    const signInWithGoogle = () => {
+        supabase.auth.signInWithOAuth({ provider: 'google' });
+    };
+
     const signOut = () => {
-		supabase.auth.signOut();
-	};
-    
+        supabase.auth.signOut();
+    };
+
     return (
-        <AuthContext.Provider value={{ user, signInWhitGithub, signOut }}>
+        <AuthContext.Provider
+            value={{
+                user,
+                SignInUser,
+                signUpNewUser,
+                signInWithGoogle,
+                signInWithGithub,
+                signOut,
+            }}
+        >
             {' '}
             {children}{' '}
         </AuthContext.Provider>
